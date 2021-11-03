faa095a663bcd17a64bedc8d634e862957549b66
phetzzy99 <phetit_sru@hotmail.com>
2021-11-03 12:52:39 +0700

    End of section 3

---
+++ client/src/features/catalog/ProductList.tsx
@@ -0,0 +1,19 @@
+import { Grid } from "@mui/material";
+import { Product } from "../../app/models/product";
+import ProductCard from "./ProductCard";
+
+interface Props {
+    products: Product[];
+}
+
+export default function ProductList({products}: Props) {
+    return (
+        <Grid container spacing={4}>
+            {products.map(product => (
+                <Grid item xs={4}>
+                    <ProductCard product={product} />
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
